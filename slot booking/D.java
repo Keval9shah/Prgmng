6e5402b3548fac711f58a3742f3f4502eeddf254
Keval Shah <skeval904@.com>
2020-05-27 22:14:30 +0530

    slb added

---
+++ slot booking/D.java
@@ -0,0 +1,99 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class D {
+
+    int Checkdate(String nm, String d, int t) throws IOException {
+
+        // Date stuff
+        String x = "", y = "", z = "";
+        int[] dat = new int[3];
+        if (d.length() > 7 && d.length() < 11) {
+            x = "";
+            int i = 0;
+            while (d.charAt(i) != '/') {
+                x += d.charAt(i);
+                i++;
+            }
+            i++;
+            y = x;
+            dat[0] = Integer.valueOf(x);
+            x = "";
+            while (d.charAt(i) != '/') {
+                x += d.charAt(i);
+                i++;
+            }
+            z = x;
+            dat[1] = Integer.valueOf(x);
+            x = "";
+            x += d.charAt(i + 1);
+            x += d.charAt(i + 2);
+            x += d.charAt(i + 3);
+            x += d.charAt(i + 4);
+            dat[2] = Integer.valueOf(x);
+            //
+
+            //
+
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy/HH");
+            String strDate = formatter.format(date);
+            String sY = "" + strDate.charAt(6) + strDate.charAt(7) + strDate.charAt(8) + strDate.charAt(9);
+            String sM = "" + strDate.charAt(3) + strDate.charAt(4);
+            String sD = "" + strDate.charAt(0) + strDate.charAt(1);
+            String sH = "" + strDate.charAt(11) + strDate.charAt(12);
+            boolean bln = true;
+            if (Integer.valueOf(sY) >= dat[2]) {
+                if (Integer.valueOf(sM) >= dat[1]) {
+                    if (Integer.valueOf(sD) >= dat[0]) {
+                        if (Integer.valueOf(sH) >= t) {
+                            return 2;
+                        } else {
+                            bln = false;
+                        }
+                    }
+                } else if (bln) {
+                    return 2;
+                }
+            } else {
+                // File stuff
+
+                //
+
+                File f = new File("C:\\LSB/data.csv");
+
+                File dir = new File("C:\\LSB");
+                dir.mkdir();
+                Path path = FileSystems.getDefault().getPath("C:\\LSB");
+                Files.setAttribute(path, "dos:hidden", true);
+                try {
+                    FileWriter wrtr = new FileWriter(f, true);
+                    wrtr.append(nm);
+                    wrtr.append(',');
+                    wrtr.append(y);
+                    wrtr.append(',');
+                    wrtr.append(z);
+                    wrtr.append(',');
+                    wrtr.append(x);
+                    wrtr.append(',');
+                    wrtr.append(String.valueOf(t));
+                    wrtr.append('\n');
+                    wrtr.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            return 3;
+        }
+
+        return 1;
+    }
+}
---
+++ slot booking/X.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * X
+ */
+public class X {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("\nLibrary slot booking\n");
+
+        boolean cont = true;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (cont) {
+
+            System.out.println("Enter your name");
+
+            String name = sc.nextLine();
+
+            System.out.println("Enter date in dd/mm/yyyy format");
+            String date = sc.nextLine();
+
+            System.out.println("Enter time in 24 hr format");
+            int time = sc.nextInt();
+
+            D d = new D();
+            int intmdt = d.Checkdate(name, date, time);
+            if (intmdt == 2) {
+                System.out.println("Do you have Time Machine?");
+                System.out.println("Bcuz You wanna book slot in past");
+            } else if (intmdt == 1) {
+                System.out.println("Done!!\nOn " + date + " at " + time);
+            } else if (intmdt == 3) {
+                System.out.println("Date format incorrect");
+            } else {
+                System.out.println("Sorry\nNo slot available on that day & that time");
+            }
+            System.out.println("\n\nEnter 1 to continue or anything else to exit");
+            int contn = sc.nextInt();
+            sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            if (contn == 1) {
+                cont = true;
+            } else {
+                cont = false;
+            }
+        }
+
+        sc.close();
+    }
+}
